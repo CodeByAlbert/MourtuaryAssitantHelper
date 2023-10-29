e6346f68916da98d0ef31b6a3c9ea44866813506
Albert Rodriguez <rdrgz1992@gmail.com>
2023-10-29 12:51:34 -0400

    Fixed Demon list display and + clear sigil btn

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 //Demon Data
 const demons = [
   { name: "Balmet", sigils: ["sigil1", "sigil2", "sigil3", "sigil4"] },
-  { name: "Anek", sigils: ["Sigil5", "Sigil6", "sigil7", "Sigil2"] },
+  { name: "Anek", sigils: ["sigil5", "sigil6", "sigil7", "sigil2"] },
 ];
 
 let selectedSigils = [];
 
-//functyion to add sigils
+// function to add sigils
 function addSigil(sigil) {
+  console.log(`Adding sigil: ${sigil}`); // Debug log
+
   if (!selectedSigils.includes(sigil)) {
     selectedSigils.push(sigil);
   }
@@ -15,19 +17,30 @@ function addSigil(sigil) {
   filterDemons();
 }
 
-//function to filter demons through selected sigils
-
-function filterDemons() {
-  const filterDemons = demons.filter((demon) => {
-    return selectedSigils.some((sigil) => demon.sigils.includes(sigil));
-  });
-
-  displayDemons(filterDemons);
+function clearSigils() {
+  selectedSigils = [];
+  displayDemons(demons);
 }
 
-//function to display demon list
+// function to filter demons through selected sigils
+function filterDemons() {
+  console.log(`Filtering with sigils: ${selectedSigils.join(", ")}`); // Debug log
 
+  const filteredDemons = demons.filter((demon) => {
+    return selectedSigils.every((sigil) => demon.sigils.includes(sigil));
+  });
+
+  console.log(
+    `Filtered demons: ${filteredDemons.map((d) => d.name).join(", ")}`
+  ); // Debug log
+
+  displayDemons(filteredDemons);
+}
+
+// function to display demon list
 function displayDemons(demonList) {
+  console.log(`Displaying demons: ${demonList.map((d) => d.name).join(", ")}`); // Debug log
+
   const demonNamesDiv = document.getElementById("demonNames");
   demonNamesDiv.innerHTML = "";
 
@@ -35,3 +48,6 @@ function displayDemons(demonList) {
     demonNamesDiv.innerHTML += `<p>${demon.name}</p>`;
   });
 }
+
+// Initial call to display all demons
+displayDemons(demons);
